6857debc52f705f42e94042dd0a27de7ff89cdd0
tvviem <trieuvinhviem@gmail.com>
2023-02-13 14:54:18 +0700

    2 - Optional Parameters for routing

---
+++ src/routes/marketing/[[lang]]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	const greetings = {
+		en: 'Hello!',
+		es: 'Hola!',
+		fr: 'Bonjour!'
+	};
+	const { lang = 'en' } = $page.params;
+	const greeting = greetings[lang as keyof typeof greetings]; // typescript
+</script>
+
+{#if greeting}
+	<h1>{greeting}</h1>
+{:else}
+	<h1>NO Greeting for lang [{lang}]</h1>
+{/if}
